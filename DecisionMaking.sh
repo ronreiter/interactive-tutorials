e4bce534806c50056305260289cae2982eddff6b
fuNi <phaneendran.gitam@gmail.com>
2014-12-04 15:11:00 +0400

    Create DecisionMaking.sh
    Now all of the conditions are evaluated to True

---
+++ DecisionMaking.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# change these variables
+# The values of the variables have been chnaged  to make all of the below conditions True!
+NUMBER=16 
+APPLES=16
+KING=LUIS
+
+if [ $NUMBER -gt 15 ] ; then
+  echo 1
+fi
+if [ $NUMBER -eq $APPLES ] ; then
+  echo 2
+fi
+if [[ ($APPLES -eq 12) || ($KING = "LUIS") ]] ; then
+  echo 3
+fi
+if [[ $(($NUMBER + $APPLES)) -le 32 ]] ; then
+  echo 4
+fi
